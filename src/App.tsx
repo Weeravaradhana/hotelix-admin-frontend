f7777ab562f9b494ce6c94b0902eda401a1f4093
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-02-01 15:49:15 +0530

    chore: configure Vite dev server port to 3000

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Layout from "./component/Layout.tsx";
+import {Navigate, Route, Routes} from "react-router-dom";
+import TenantCreate from "./pages/tenants/TenantCreate.tsx";
+import TenantList from "./pages/tenants/TenantList.tsx";
+import TenantDetails from "./pages/tenants/TenantDetails.tsx";
+import TenantEdit from "./pages/tenants/TenantEdit.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<Layout>
+    <Routes>
+        <Route path="/" element={<Navigate to="/tenants" replace />} />
+        <Route path="/tenants/create" element={<TenantCreate />} />
+        <Route path="/tenants" element={<TenantList />} />
+        <Route path="/tenants/:tenantId" element={<TenantDetails />} />
+        <Route path="/tenants/:tenantId/edit" element={<TenantEdit />} />
+    </Routes>
+</Layout>
+
+
     </>
   )
 }
